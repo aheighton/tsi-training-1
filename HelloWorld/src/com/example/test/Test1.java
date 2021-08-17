9319a45f5ba2f61e6ee2b18aea00312bf5612056
Alex Heighton <heiga002@hotmail.co.uk>
2021-08-17 15:02:23 +0100

    Cleanup and validity check

--- HelloWorld/src/com/example/test/Test1.java
+++ HelloWorld/src/com/example/test/Test1.java
@@ -19,20 +19,26 @@ public class Test1 {
 
     public static void passwordChecker(String password)
     {
+        boolean isValid = true;
+
+
         if (password.equals(password.toUpperCase()))
         {
             System.out.println("Must contain lower case letter.");
+            isValid = false;
         }
 
         if (password.equals(password.toLowerCase()))
         {
             System.out.println("Must contain upper case letter.");
+            isValid = false;
         }
 
         boolean containsNumber = false;
         for (int checkNumber = 0; checkNumber < 10; checkNumber++)
         {
-            if (password.contains(String.valueOf(checkNumber))) {
+            if (password.contains(String.valueOf(checkNumber)))
+            {
                 containsNumber = true;
                 break;
             }
@@ -40,12 +46,18 @@ public class Test1 {
         if (!containsNumber)
         {
             System.out.println("Must contain number 0-9.");
+            isValid = false;
         }
 
         if (password.length() < 8)
         {
             System.out.println("Must be at least 8 characters long.");
+            isValid = false;
         }
 
+        if (isValid)
+        {
+            System.out.println("Password valid.");
+        }
     }
 }
--- HelloWorld/src/com/example/test/Test2.java
+++ HelloWorld/src/com/example/test/Test2.java
@@ -1,7 +1,9 @@
 package com.example.test;
 
-public class Test2 {
-    public static void main(String[] args){
-        System.out.println("Testing");
+public class Test2
+{
+    public static void main(String[] args)
+    {
+
     }
 }
