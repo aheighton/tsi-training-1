1699dcf72da98714cf7c33e0b7af64f54496ad53
Alex Heighton <heiga002@hotmail.co.uk>
2021-08-18 13:48:53 +0100

    Password scoring procedure using regular expressions

---
+++ HelloWorld/src/com/example/test/PasswordScore.java
@@ -0,0 +1,36 @@
+package com.example.test;
+
+import java.util.regex.*;
+
+public class PasswordScore
+{
+	public static void main(String[] args)
+	{
+		String newPass = Test1.passwordInput();
+		int passScore = calcPassScore(newPass);
+		System.out.println("Your password's score is: "+passScore);
+		if (passScore < 10) System.out.println("Not great. You can do better.");
+		else if (passScore < 20) System.out.println("Good! Not great, but pretty good!");
+		else if (passScore < 40) System.out.println("Better, pretty secure! " +
+				"Make sure you use different passwords for every account.");
+		else System.out.println("Excellent! You really know your stuff.");
+	}
+
+	public static int calcPassScore(String password)
+	{
+		if (password.length() < 8) return 0;
+
+		Pattern alpha = Pattern.compile("[abcdefghijklmnopqrstuvwxyz]");
+		Pattern alphaUp = Pattern.compile("[ABCDEFGHIJKLMNOPQRSTUVWXYZ]");
+		Pattern number = Pattern.compile("[0123456789]");
+		Pattern special = Pattern.compile("[!£$%^&*()#;'?/.,:@~{}]");
+
+		int multiplier = 0;
+		if (alpha.matcher(password).find()) multiplier += 1;
+		if (alphaUp.matcher(password).find()) multiplier += 1;
+		if (number.matcher(password).find()) multiplier += 1;
+		if (special.matcher(password).find()) multiplier += 1;
+
+		return (multiplier * (password.length()-2));
+	}
+}
